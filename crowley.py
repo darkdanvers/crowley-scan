9cb53a39c1fe70de4ce94931c8099935182a7e4d
Gabriel Dutra <root@bsd0x.me>
2020-06-20 18:26:09 -0300

    Crowley: add first version

---
+++ crowley.py
@@ -0,0 +1,22 @@
+import click
+
+from engines import google
+from libs.logging import banner
+from vull import sql_injection
+
+@click.command()
+@click.option('--dork', nargs=1)
+def main(dork):
+
+    search = google.SearchGoogle(dork)
+    google_results = search.search_results()
+    sqli = sql_injection.SqlInjection()
+
+    for target in google_results:
+        sqli.check_vull(target)
+    
+    sqli.report()
+
+if __name__=='__main__':
+    banner()
+    main()
