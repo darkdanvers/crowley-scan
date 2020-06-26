f1024780dea30818f79d647d4b4ce18e61e9d14c
bsd0x <root@bsd0x.me>
2020-06-26 00:18:36 -0300

    Remove log warning, log danger and create full report

--- libs/logging.py
+++ libs/logging.py
@@ -1,12 +1,19 @@
 def log_info(msg):
     print("\033[1;92m[INFO] {0}\033[0;0m".format(msg))
 
-def log_danger(msg):
-    print("\033[1;91m[DANGER] {0}\033[0;0m".format(msg))
-
-def log_warning(msg):
-    print("\033[1;93m[WARNING] {0}\033[0;0m".format(msg))
+def log_error(msg):
+    print("\033[1;91m[ERROR] {0}\033[0;0m".format(msg))
 
 def log_vulnerable(msg):
     print("\033[1;93m[VULL] {0}\033[0;0m".format(msg))
 
+def log_report_targets_vulnerables(target, engine, http_status_code, vulnerabilities):
+    print("-" * 79)
+    
+    output = '''\033[1;93m
+Target: {0}
+Engine: {1}
+HTTP Status code: {2}
+Vulnerabilitie: {3}\033[0;0m'''.format(target, engine, http_status_code, vulnerabilities)
+
+    print(output)
