7d8967b0137c17dc93d4d0737124dc8ea99fa9a0
bsd0x <root@bsd0x.me>
2020-06-28 16:30:29 -0300

    Add support to multithreads

--- crowley.py
+++ crowley.py
@@ -26,21 +26,26 @@ from engines import google
 from libs import banner
 from vull import sql_injection
 from vull import reporting
+from multiprocessing.dummy import Pool as ThreadPool
 
 @click.command()
 @click.option('--dork', nargs=1)
 @click.option('--max_results', default=100, nargs=1, type=click.INT)
 @click.option('--timeout', default=3, nargs=1, type=click.INT)
-def main(dork, max_results, timeout):
+@click.option('--threads', default=1, nargs=1, type=click.INT)
+def main(dork, max_results, timeout, threads):
 
     search = google.SearchGoogle(dork, max_results, timeout)
     google_results = search.search_results()
     sqli = sql_injection.SqlInjection()
     report = reporting.ReportVulnerabilities()
 
-    for target in google_results:
-        sqli.check_vull(target)
-    
+    thread_pool = ThreadPool(threads)
+
+    results = thread_pool.map(sqli.check_vull, google_results)
+    thread_pool.close()
+    thread_pool.join()
+
     report.create_report()
 
 if __name__=='__main__':
